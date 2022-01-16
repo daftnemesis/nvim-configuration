e5c0d0b6b9fda7e28e236841783c076b1fbce1ba
Jose Lozano <XxdaftnemesisxX@gmail.com>
2022-01-15 21:55:25 -0600

    adding git-signs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,6 +94,14 @@ return packer.startup(function(use)
   -- Colors
   use 'norcalli/nvim-colorizer.lua'
 
+  -- Git
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -114,6 +114,11 @@ _G.packer_plugins = {
     path = "/home/nemesis/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
+  ["gitsigns.nvim"] = {
+    loaded = true,
+    path = "/home/nemesis/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    url = "https://github.com/lewis6991/gitsigns.nvim"
+  },
   ["lualine.nvim"] = {
     loaded = true,
     path = "/home/nemesis/.local/share/nvim/site/pack/packer/start/lualine.nvim",
