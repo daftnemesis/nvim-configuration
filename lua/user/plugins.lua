9dc70a0a81f15ad1660cb252aef4d4d6642199f1
Jose Lozano <XxdaftnemesisxX@gmail.com>
2022-01-15 22:04:10 -0600

    adding project plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -102,6 +102,9 @@ return packer.startup(function(use)
     },
   }
 
+  -- Project
+  use "ahmedkhalf/project.nvim" 
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -190,6 +190,11 @@ _G.packer_plugins = {
     path = "/home/nemesis/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["project.nvim"] = {
+    loaded = true,
+    path = "/home/nemesis/.local/share/nvim/site/pack/packer/start/project.nvim",
+    url = "https://github.com/ahmedkhalf/project.nvim"
+  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/home/nemesis/.local/share/nvim/site/pack/packer/start/telescope.nvim",
